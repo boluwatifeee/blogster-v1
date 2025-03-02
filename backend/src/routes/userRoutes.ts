f3d0aa49ec7a17694fb3e37f54ad9b594b9911b0
Boluwatife Ladeinde <ladeindeb@gmail.com>
2025-03-02 21:18:50 +0100

    changes to routes file

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,9 +4,10 @@ import { protect, admin } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
+router.post("/auth", authUser);
 // router.route("/").post(registerUser).get(protect, admin, getUsers);
 // router.post("/logout", logoutUser);
-router.post("/auth", authUser);
+
 // router
 //   .route("/profile")
 //   .get(protect, getUserProfile)
